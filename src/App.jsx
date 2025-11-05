ac28ea2d61cb33ebad9fc18336a11425a0a095fc
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-05 20:14:20 +0900

    🔥 Remove: 필요 없는 파일 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import "./App.css";
-
 function App() {
-  return (
-    <>
-      <title>타이틀 적용해보기~!</title>
-      <meta name="description" content="웹 페이지 설명 적용해보기~" />
-      <h1>테스트~</h1>
-    </>
-  );
+  return <div>form 연습하기</div>;
 }
 
 export default App;
