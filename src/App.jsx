b5e70ef4adc84b8983891101d335fa44de51b5eb
apple <apple@apples-MacBook-Air.local>
2025-11-10 02:27:25 +0900

    tailwind.config.js 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import BioForm from './components/Bio';
+import EmailForm from './components/Email';
+import PasswordForm from './components/Password';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <EmailForm />
+      <PasswordForm />
+      <BioForm />
+    </>
+  );
 }
 
 export default App;
