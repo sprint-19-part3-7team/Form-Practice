72d556e01bd531b14b5224b488ab10057f3e91c7
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-11 23:54:44 +0900

    ✨ Feat: 에러 컨텍스트 추가 및 에러 스타일 추가

--- src/components/common/input/Input.jsx
+++ src/components/common/input/Input.jsx
@@ -1,11 +1,15 @@
-const Input = ({ id, name, placeholder, type = 'text', ...props }) => {
+import { useContext } from 'react';
+
+const Input = ({ id, name, placeholder, type = 'text', context, ...props }) => {
+  const { error } = useContext(context);
+
   return (
     <input
       id={id}
       name={name}
       placeholder={placeholder}
       type={type}
-      className='h-14 w-full rounded-lg border border-gray-200 px-4 placeholder:text-gray-300'
+      className={`h-14 w-full rounded-lg border px-4 placeholder:text-gray-300 ${error ? 'border-error' : 'border-gray-200'}`}
       {...props}
     />
   );
