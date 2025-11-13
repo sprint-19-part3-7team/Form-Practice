d986e28efab38c2aab429941a5a4bce3557406d0
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-13 20:31:43 +0900

    ✨ Feat: EmailField, PasswordField 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import FormField from '@/components/form/form-field/FormField';
-import { FormFieldContext } from '@/components/form/form-field/formFieldContext';
-
 const App = () => {
   return (
     <div className='px-4'>
-      <form className='mx-auto max-w-180 py-25'>
-        <FormField error={'이메일 형식에 맞게 작성해주세요.'}>
-          <FormField.Label htmlFor='email' required>
-            이메일
-          </FormField.Label>
-          <FormField.Input
-            id='email'
-            name='email'
-            type='email'
-            placeholder='이메일을 입력해주세요.'
-            autoComplete='email'
-            context={FormFieldContext}
-            required
-          />
-          <FormField.ErrorMsg context={FormFieldContext} />
-        </FormField>
+      <form className='mx-auto flex max-w-180 flex-col gap-10 py-25'>
+        <FormField.Email />
+        <FormField.Password />
       </form>
     </div>
   );
